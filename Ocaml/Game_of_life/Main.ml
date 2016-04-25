74f14e4eb6b111126f428f7f2249827426d839f7
Hellpi Mappa <baudetad@gmail.com>
2016-04-25 11:01:46 +0200

    Mise a jour

--- Ocaml/Game_of_life/Main.ml
+++ Ocaml/Game_of_life/Main.ml
@@ -1,17 +1,8 @@
-(*
-let to_char x = if x then "X" else "."
-let show_tab tableau = 
-	let tableaubis = Array.map (function x -> String.concat "" (Array.map to_char x)) tableau
-	in List.iter print_endline tableaubis
-*)
 let visualise x = if x then "X" else "."
 let liste_make i element =
 	let rec aux acc = function
 		|x when x = i -> acc
 		|x -> aux (element :: acc) (x + 1)
-	in List.rev (aux [] 0)
-let test = liste_make 20 false
-(*
-let tableau_base = Array.make 20 (Array.make 20 false)
-let _ = show_tab tableau_base
-*)
+	in aux [] 0
+let test = List.map (function x -> Random.bool()) (liste_make 20 false)
+let _ = List.iter (function x -> print_endline (string_of_bool x)) test
