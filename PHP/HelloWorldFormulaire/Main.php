c2b9c3a801fb858630ca1dbc10326ef88d8c9ff8
Hellpi Mappa <baudetad@gmail.com>
2016-06-06 14:28:58 +0200

    Mise a jour

---
+++ PHP/HelloWorldFormulaire/Main.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Test PHP</title>
+	</head>
+	<body>
+		<form action="Action.php" method="post">
+			<p>Votre nom : <input type="text" name="nom" /></p>
+			<p><input type="submit" value="OK"></p>
+		</form>
+	</body>
+</html>
---
+++ PHP/HelloWorldFormulaire/README.md
@@ -0,0 +1,3 @@
+# Hello world via formulaire
+Un simple Hello world en PHP ou il faut rentrer son nom dans un formulaire.
+[Voici une demo](http://kustomskromatiks.byethost17.com/Experiences/BonjourFormulaire.php)
