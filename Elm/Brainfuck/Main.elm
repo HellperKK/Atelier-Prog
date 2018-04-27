15824971e3504b4470e19f10df07ca7191be1354
François Berhn <baudetad@gmail.com>
2018-04-27 10:34:03 +0200

    Started parser

--- Elm/Brainfuck/Main.elm
+++ Elm/Brainfuck/Main.elm
@@ -4,7 +4,7 @@ import Html.Events
 import String
 import Dict
 import Random
-
+import Array
 
 
 main =
@@ -35,21 +35,58 @@ model = { code = ""
 
 
 -- UPDATE
-
-
 type Msg
   = Update_code String
   | Update_entry String
+  | Interpret
+
+type P_Code
+  = Move_Right
+  | Move_Left
+  | Increment
+  | Decrement
+  | Print
+  | Input
+  | Loop List P_Code
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Update_code str -> {code=str, entry=model.entry, output=str}
+    Update_code str -> {code=str, entry=model.entry, output=model.output}
     Update_entry str -> {code=model.code, entry=str, output=model.output}
+    Interpret -> {code=model.code, entry=model.entry, output=(interpret model.code model.entry)}
 
+interpret code entry =
+  let
+    pseudo_code = parse code
+  in
+    code
 
+parse code =
+  let
+    aux acc new_str =
+      case String.uncons new_str of
+        Nothing -> acc
+        Just ('>', tl) -> aux (Move_Right :: acc) tl
+        Just ('<', tl) -> aux (Move_Left :: acc) tl
+        Just ('+', tl) -> aux (Increment :: acc) tl
+        Just ('-', tl) -> aux (Decrement :: acc) tl
+        Just ('.', tl) -> aux (Print :: acc) tl
+        Just (',', tl) -> aux (Input :: acc) tl
+        Just ('[', tl) -> aux (Input :: acc) tl
+        Just (_, tl) -> aux acc tl
+  in
+    aux [] code
 
+may_increment arr index =
+  case Array.get index arr of
+    Just x -> Array.set index (x+1) arr
+    Nothing -> arr
 
+may_decrement arr index =
+  case Array.get index arr of
+    Just x -> Array.set index (x-1) arr
+    Nothing -> arr
 
 
 -- VIEW
@@ -64,5 +101,5 @@ view model =
       , Html.br [] []
       , Html.textarea [Html.Events.onInput Update_entry] []
       , Html.br [] []
-      -- , Html.button [onClick (Entree 2)] [Html.text "Ciseaux"]
+      , Html.button [Html.Events.onClick Interpret] [Html.text "Interpreter"]
       ]
